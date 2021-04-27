16ca86c2ad0f43f4cb79e0f5be85be2b8fce51fd
Dor <dorzairi@ymail.com>
2021-04-26 21:18:01 -0400

    fix

--- Task1/Anonymous.js
+++ Task1/Anonymous.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const anonSchema = new Schema({
-  anonData: { type: Schema.Types.anonData }
+  anonData: String
 });
 
 const memeSchema = new Schema({
   name: String,
   accessURL: String,
-  anonSchema: anonSchema
+  anonSchema: { type: Schema.Types.anonData }
 });
 
 module.exports = mongoose.model("Meme", memeSchema);
