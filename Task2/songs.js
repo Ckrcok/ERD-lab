a082162c91147e4d516a68e1708a45eba4413a66
Dor <dorzairi@ymail.com>
2021-04-26 20:57:27 -0400

    Done songs model, forgot the export on playlist

---
+++ Task2/songs.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const songSchema = new Schema({
+  title: String,
+  artist: String,
+  album: { type: Schema.Types.ObjectId, ref: "Album" }
+});
+
+module.exports = mongoose.model("Song", songSchema);
